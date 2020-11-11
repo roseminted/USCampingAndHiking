68153921582c0be09e622298cc0c202f5281b653
roseminted <57846775+roseminted@users.noreply.github.com>
2020-11-10 22:11:36 -0500

    change maps style

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -2,7 +2,7 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'cluster-map',
-    style: 'mapbox://styles/mapbox/outdoors-v11',
+    style: 'mapbox://styles/mapbox/light-v10',
     center: [-103.59179687498357, 40.66995747013945],
     zoom: 3
 });
